cfaab0a785b3c9b256dd317d4c8b525e072e27eb
akashyellgetti <akash.yellgetti@bts.com>
2026-08-24 21:09:51 +0530

    udpate certbot

---
+++ docker-certbot.sh
@@ -0,0 +1,11 @@
+docker run --rm \
+  -v "$(pwd)/containers/certbot/letsencrypt:/etc/letsencrypt" \
+  -v "$(pwd)/containers/certbot/www:/var/www/certbot" \
+  certbot/certbot:latest \
+  certonly \
+  --webroot \
+  -w /var/www/certbot \
+  --email ayellgetti@gmail.com \
+  --agree-tos \
+  --no-eff-email \
+  -d myfinancefreedom.com
