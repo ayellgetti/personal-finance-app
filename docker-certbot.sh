f37544151fe8ec69c8eb15587541c2bbb6ceabd1
akashyellgetti <akash.yellgetti@bts.com>
2026-08-31 14:43:30 +0530

    hi

--- docker-certbot.sh
+++ docker-certbot.sh
@@ -1,3 +1,9 @@
+#!/bin/sh
+# Issue or expand a single Let's Encrypt cert for nginx.prod.conf.
+# HTTP-01 needs nginx serving /.well-known/acme-challenge/ from
+# containers/certbot/www, and each -d name must resolve to this host.
+set -e
+
 docker run --rm \
   -v "$(pwd)/containers/certbot/letsencrypt:/etc/letsencrypt" \
   -v "$(pwd)/containers/certbot/www:/var/www/certbot" \
@@ -8,7 +14,10 @@ docker run --rm \
   --email ayellgetti@gmail.com \
   --agree-tos \
   --no-eff-email \
+  --non-interactive \
+  --expand \
+  --cert-name myfinancefreedom.com \
   -d myfinancefreedom.com \
-  -d www.myfinancefreedom.com
-  -d api.myfinancefreedom.com
+  -d www.myfinancefreedom.com \
+  -d api.myfinancefreedom.com \
   -d web.myfinancefreedom.com
